244d2b0c781e4e65feea3236c48e3700e6f43d9d
Gihyun <giordnnuz@gmail.com>
2023-07-13 14:05:15 +0800

    Refactored code + Sort by size

--- src/main.rs
+++ src/main.rs
@@ -2,63 +2,105 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
-fn main() {
-    let mut dir_path_input = String::new();
-
-    println!("Enter path:\n");
-
-    io::stdin()
-        .read_line(&mut dir_path_input)
-        .expect("Invalid input");
-
-    let dir_path = dir_path_input.replace("\\", "/").trim().to_string();
-
-    // println!("{}", dir_path);
-    println!();
-
-    let read_dir = fs::read_dir(dir_path);
-
-    match read_dir {
-        Ok(entries) => {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    let file_path = entry.path();
-                    let file_path_str = file_path.to_string_lossy().replace("\\", "/");
-                    let file_name = entry.file_name();
-
-                    get_metadata(&file_path);
-
-                    if let Some(name) = file_name.to_str() {
-                        println!("{}", name);
-                    }
-
-                    println!("{}\n", file_path_str);
-                }
-            }
-        }
-        Err(err) => {
-            eprintln!("Error: {}", err);
-            return;
-        }
-    }
+struct FileData {
+    name: String,
+    path: String,
+    size: f64,
 }
 
-fn get_metadata(path: &Path) {
-    if path.is_file() {
-        let metadata = fs::metadata(path).unwrap();
+fn main() {
+    let dir_path = read_user_input("Enter path:");
 
-        let size_in_kb = metadata.len() as f64 / (1024.0);
+    let mut files: Vec<FileData> = match get_file_data(&dir_path) {
+        Ok(data) => data.into_iter().collect(),
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            Vec::new() // or handle the error case as needed
+        }
+    };
+    
+    files.sort_by(|a, b| b.size.partial_cmp(&a.size).unwrap());
 
-        if size_in_kb < 1024.0 {
-            println!("Size: {:.2} KB", size_in_kb);
-        } else if size_in_kb < 1024.0 * 1024.0 {
-            let size_in_mb = size_in_kb / 1024.0;
+    for file in &files {
+        println!("{}", file.name);
+        println!("{}", file.path);
+        println!("{:.2} KB\n", file.size);
+    }
 
-            println!("Size: {:.2} MB", size_in_mb);
+    println!("Total of {} files.\n", files.len());
+}
+
+fn read_user_input(prompt: &str) -> String {
+    println!("{}", prompt);
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Invalid input");
+
+    input.trim().replace('\\', "/")
+}
+
+fn get_file_data(dir_path: &str) -> io::Result<Vec<FileData>> {
+    let entries = fs::read_dir(dir_path)?;
+
+    let mut file_data = Vec::new();
+    
+    for entry in entries {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+        let path = entry.path();
+
+        let data = if file_type.is_file() {
+            let size_in_kb: f64 = get_file_size(&path).unwrap();
+            let name = entry.file_name().to_string_lossy().to_string();
+
+            FileData {
+                name,
+                path: path.to_string_lossy().replace('\\', "/"),
+                size: size_in_kb,
+            }
+        } else if file_type.is_dir() {
+            let size_in_kb: f64 = get_directory_size(&path)?;
+            let name = entry.file_name().to_string_lossy().to_string();
+
+            FileData {
+                name,
+                path: path.to_string_lossy().replace('\\', "/"),
+                size: size_in_kb,
+            }
         } else {
-            let size_in_gb = size_in_kb / (1024.0 * 1024.0);
+            continue; // Skip non-file and non-directory entries
+        };
 
-            println!("Size: {:.2} GB", size_in_gb);
+        file_data.push(data);
+    }
+
+    Ok(file_data)
+}
+
+fn get_file_size(path: &Path) -> io::Result<f64> {
+    let metadata = fs::metadata(path)?;
+    let size_in_bytes = metadata.len();
+
+    Ok(size_in_bytes as f64 / 1024.0)
+}
+
+fn get_directory_size(path: &Path) -> io::Result<f64> {
+    let mut total_size: f64 = 0.0;
+    let entries = fs::read_dir(path)?;
+
+    for entry in entries {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+        let entry_path = entry.path();
+
+        if file_type.is_file() {
+            let size_in_kb: f64 = get_file_size(&entry_path)?;
+            total_size += size_in_kb;
+        } else if file_type.is_dir() {
+            let size_in_kb: f64 = get_directory_size(&entry_path)?;
+            total_size += size_in_kb;
         }
     }
+
+    Ok(total_size)
 }
